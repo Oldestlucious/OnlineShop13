4e02ee1ca14d61e38cfc5287528783d2205bb44c
Oldestlucious <123902745+Oldestlucious@users.noreply.github.com>
2024-04-12 20:06:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-   
-        var lastScrollTop = 0;
-        window.addEventListener("scroll", function() {
-            var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-            if (currentScroll > lastScrollTop) {
-                // Descendre la page
-                document.querySelector('.creator-name').classList.remove('hide');
-            } else {
-                // Monter la page
-                document.querySelector('.creator-name').classList.add('hide');
-            }
-            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-        }, false);
-    
+
